c86bd7ff2bcfcc5761028f038430a61a12bc0e7f
belaid-dali <daliomar.belaid@gmail.com>
2024-04-30 13:50:45 +0100

    adding more features to our game

--- script.js
+++ script.js
@@ -9,15 +9,17 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const player0El = document.querySelector('player--0');
-const player1El = document.querySelector('player--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 // starting conditons 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // implementing - rolling the Dice feature 
 btnRoll.addEventListener('click', function() {
@@ -33,10 +35,15 @@ btnRoll.addEventListener('click', function() {
     if(dice !== 1){
         //add the dice to the current
         currentScore += dice;
-        current0El.textContent = currentScore; //to be changed later 
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+ 
     }else {
-    // switch to next player
-
+        // switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0; 
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 
 })
